2d500f277319cfbd7070f676e97e609a5173c235
mebi-wz <mehbubadilla@gmail.com>
2026-05-04 18:25:08 +0300

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { ArrowRight, Mail, Phone, MapPin } from 'lucide-react'
-import logo from '../assets/logo.png'
+import logo from '../assets/logo_transparent.png'
 import './Footer.css'
 
 const TwitterIcon = () => (
