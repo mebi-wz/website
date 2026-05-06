e9ea5b82a99d553abe758f958ec2e3e205b0a929
mebi-wz <mehbubadilla@gmail.com>
2026-05-06 15:31:02 +0300

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
                 <MapPin size={20} color="var(--accent-primary)" />
                 <div>
                   <strong>Location &amp; Hours</strong>
-                  <p>Airport Road, Addis Ababa<br/>Mon-Sat: 8:00am To 5:00pm</p>
+                  <p>Airport Road, Addis Ababa<br />Mon-Sat: 8:00am To 5:00pm</p>
                 </div>
               </div>
             </div>
